8e9667f66e3f319845c52f458606ce22bfde3b7c
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-04-24 20:26:50 +0900

    Set up configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                use: [
+                    'file-loader'
+                ]
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
