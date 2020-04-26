1986adc07f398f1116fe36747f7cd26208726547
Konstantin Marinov <marinov.kohctahtih@gmail.com>
2020-04-26 19:17:35 +0900

    Separated dev and prod builds

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
